7cf885dcee0b197fad76a2e96e223a88f4d373ef
German Nr4 <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2020-09-04 19:59:03 +0200

    Update 2 - 04/09/20
    Update 2 - 04/09/20
    -Added social_class.py
    -Restructured Population Simulation.py
    -Updated Population Simulation.py
    -Added three(3) different speed for the simulation
    -Added Code Test.py to test code pieces
    
    --See additonal Changelog for last three(3) updated below--
    
    Update - 04/09/20
    -Created UI Code to test GUI
    -Updated Population Simulation.py
    
    Update - 01/09/20
    -Added Code Dump.txt for code that isn't needed but could become useful
    -Updated Population Simulation.py
    
    Original Upload - 31/08/20
    -Uploaded Population Simulation.py

--- Population Simulator.py
+++ Population Simulator.py
@@ -1,40 +1,47 @@
 #Add imports here
 import time
 from random import *
+from social_class import *
+
+#Simulation Status
+running = True
+go_fast = False
+go_very_fast = True
 
 #Time
 year = 1
 decade_counter = 1
 end_year = 100
 
-#Population
-r = 0.5  #(r = Growth) modifie this number to increase growth speed
-K = 2000 #Maximum supstinable population
-euler = 2.71828
-    
-Gen_0 = 0 #Newborn
-Gen_1 = 0 #Children
-Gen_2 = 90 #Young Adults
-Gen_3 = 10 #Adults
-Gen_4 = 0 #Seniors
+#Population and Social Classes
+#Plebs
+social_class.plebs = social_class()
 
-New_P = 0
-P = 1500
-P2 = 0
-Death = 0
+social_class.plebs.r = 0.5  #(r = Growth) modifie this number to increase growth speed
+social_class.plebs.K = 2000 #Maximum supstinable population
+   
+social_class.plebs.gen_0 = 0 #Newborn
+social_class.plebs.gen_1 = 0 #Children&Teens
+social_class.plebs.gen_2 = 90 #Young Adults
+social_class.plebs.gen_3 = 10 #Adults
+social_class.plebs.gen_4 = 0 #Seniors
 
-randomDeaths = 0
-Childdeaths = 0
-Growth = 0
+social_class.plebs.new_P = 0
+social_class.plebs.P = 100
+social_class.plebs.old_P = 0
+social_class.plebs.randomDeaths = 0
+social_class.plebs.growth = 0
+
+death = 0
 
 #Other
-running = True
+e = 2.71828
 
 #Start Numbers get printed
-P = Gen_0+Gen_1+Gen_2+Gen_3+Gen_4
+social_class.plebs.P = social_class.plebs.gen_0+social_class.plebs.gen_1+social_class.plebs.gen_2+social_class.plebs.gen_3+social_class.plebs.gen_4
 print("Year: "+ str(year))
-print("Population: " + str(P))
-print("Growth: "+str(Growth))
+print("Population: " + str(social_class.plebs.P))
+print("Growth: "+str(social_class.plebs.growth))
 
 while running == True:
 
@@ -46,68 +53,58 @@ while running == True:
     if decade_counter == 10:
 
         #Population
-        P2 = P
-        P = round(K/(1+((K-P)/P*euler**(r*10))))
+        social_class.plebs.old_P = social_class.plebs.P
+        social_class.plebs.P = round(social_class.plebs.K/(1+((social_class.plebs.K-social_class.plebs.P)/social_class.plebs.P*e**(social_class.plebs.r*10))))
     
         #Get amount of new pop
-        New_P = P2-P #Hope this works!
+        social_class.plebs.new_P = social_class.plebs.old_P-social_class.plebs.P #Hope this works!
         
-        randomDeaths = randint(0,10)
-        randomDeaths = randomDeaths/10
-        randomDeaths = round(randomDeaths, 2)
-        randomDeaths = New_P*randomDeaths
-        New_P = round(New_P-randomDeaths)
+        social_class.plebs.randomDeaths = randint(0,10)
+        social_class.plebs.randomDeaths = social_class.plebs.randomDeaths/10
+        social_class.plebs.randomDeaths = round(social_class.plebs.randomDeaths, 2)
+        social_class.plebs.randomDeaths = social_class.plebs.new_P*social_class.plebs.randomDeaths
+        social_class.plebs.new_P = round(social_class.plebs.new_P-social_class.plebs.randomDeaths)
         
         #Move Gens
-        Death = Gen_4
-        Gen_4 = Gen_3
-        Gen_3 = Gen_2
-        Gen_2 = Gen_1
-        Gen_1 = Gen_0
-        Gen_0 = New_P
+        social_class.plebs.death = social_class.plebs.gen_4
+        social_class.plebs.gen_4 = social_class.plebs.gen_3
+        social_class.plebs.gen_3 = social_class.plebs.gen_2
+        social_class.plebs.gen_2 = social_class.plebs.gen_1
+        social_class.plebs.gen_1 = social_class.plebs.gen_0
+        social_class.plebs.gen_0 = social_class.plebs.new_P
 
         decade_counter = 0
 
-        P = Gen_0+Gen_1+Gen_2+Gen_3+Gen_4
-        Growth = New_P-Death
-        
-        
-
-    #elif decade_counter == 5:
-    #    Death = (Gen_4/2)+(Gen_3/4)
-    #    Gen_3 = Gen_3/4
-    #    Gen_3 = round(Gen_3)
-    #    Gen_4 = Gen_4/2
-    #    Gen_4 = round(Gen_4)
-    #    Death = round(Death)
-    #    
-    #    #Update P
-    #    P = Gen_0+Gen_1+Gen_2+Gen_3+Gen_4
-
+        social_class.plebs.P = social_class.plebs.gen_0+social_class.plebs.gen_1+social_class.plebs.gen_2+social_class.plebs.gen_3+social_class.plebs.gen_4
+        social_class.plebs.growth = social_class.plebs.new_P-social_class.plebs.death
 
     else:
-        New_P = 0
-        Death = 0
-        Growth = 0
+        social_class.plebs.new_P = 0
+        social_class.plebs.death = 0
+        social_class.plebs.growth = 0
  
     #Printer goes prrrrrrrr
 
     print("Year: "+ str(year))
-    if P <= 0:
-        print("Population is Zero, nobody is left to reproduce")
-        running = False
 
     if year == end_year:
-        print("Population:" + str(P))
-        print("Growth: "+str(Growth))
+        print("Population:" + str(social_class.plebs.P))
+        print("Growth: "+str(social_class.plebs.growth))
         print("The set end year was reached and the simulation ended")
         running = False
         
     else:
-        print("Population:" + str(P))
-        print("Growth: "+str(Growth))
-        time.sleep(1)
+        print("Population:" + str(social_class.plebs.P))
+        print("Growth: "+str(social_class.plebs.growth))
+        
+        if go_fast == True:
+            time.sleep(0.5)
 
+        if go_very_fast == True:
+            time.sleep(0.1)
+            
+        else:
+            time.sleep(1)
 
 else:
     print("SIMULATION ENDED AFTER "+str(year)+" YEARS")
