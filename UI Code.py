ef0c63236b5af7dd2f189cbb160a03ac4aa2aa6e
German Nr4 <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2020-09-07 17:13:52 +0200

    GUI Update 07/09/20
    -Renamed >Population Simulator.py< to >population_sim.py<
    -Added >adults< to >social_class.py< to improve the simulation
    -Added basic GUI code

--- UI Code.py
+++ UI Code.py
@@ -2,23 +2,11 @@ from tkinter import *
 
 #tkinter.Tk(sreenName=None, baseName=None, className='Tk', useTk=1)
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+window=Tk()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+btn=Button(window, text="Test Button", fg="blue")
+btn.place(x=80, y=100)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
+window.title("Test")
+window.geometry("300x200+10+20")
+window.mainloop()
