9bb1ba734d9daa032078801b3bcd09553308248c
German Nr4 <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2020-09-13 11:02:27 +0200

    Update 13/09/20
    -Minor rewrite of >pop_sim_rework.py<
    -Moved ui code to >Code Dump - Files< Folder

--- Code Dump - Files/pop_sim_ui.py
+++ Code Dump - Files/pop_sim_ui.py
@@ -9,8 +9,7 @@ OUTPUT_2 = 0
 sg.theme('DarkAmber')	# Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text(OUTPUT_1)],
-            [sg.Text(OUTPUT_2)],
+            [sg.InputText()],
             [sg.Button('Start'), sg.Button('Cancel')] ]
      
 # Create the Window
@@ -20,8 +19,7 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':	# if user closes window or clicks cancel
         break
-    
-    if event == "Start":
-        OUTPUT_1 = 1
-    
+    print(values[0])
+    text_input = values[0]
+    sg.popup("You entered", text_input)
 window.close()
--- pop_sim_rework.py
+++ pop_sim_rework.py
@@ -1,8 +1,6 @@
 #Add imports here
 import time
-import pygame as pg
 from random import *
-from social_class import *
 import csv
 
 #Simulation Status
@@ -11,13 +9,11 @@ go_normal = False
 go_fast = False
 go_very_fast = False
 events = True
-
 #Time
 tts = 1 #Totat Timesteps
 month = 1
 year = 1
 end_year = 10
-
 #Amount of Pops & related stuff; 1 Pop = 1k People
 P = 1
 cGrowth = 0
@@ -28,22 +24,24 @@ bGrowth = 100
 default_growth = 100
 n_growth = 0
 K = 50
-rK = 50 #K+all modfiers 
 
 #Set-Up
 cGrowth = cGrowth+(bGrowth*(pg+gm-dm))
 bGrowth = default_growth*(pg+gm-dm)
 
+def write_file(tts, P, month, year):
+    #Write into save_file.txt
+    file.write(str(month)+";"+str(year)+";"+str(tts)+";"+ str(P)+'\n')
+def print_data(month, year, P, bGrowth):
+    print("Month: "+str(month)+" Year: "+ str(year))
+    print("Population: " + str(P)+"K")
+    print("Growth: "+str(bGrowth))
+
 localtime = time.asctime( time.localtime(time.time()) )
 file =open ("save_file.csv", "w", newline='')
-file.write("Simulation from the "+str(localtime)+";;Total Timesteps;"+'\n')
-file.write("Month: "+str(month)+" Year: "+ str(year)+';')
-file.write("Population: "+";"+str(tts)+";"+ str(P)+'\n')
-#file.write("Population: "+";"+ str(P)+";"+str(tts)+'\n')
-print("Month: "+str(month)+" Year: "+ str(year))
-print("Population: " + str(P)+"K")
-print("Growth: "+str(bGrowth))
-#print("Pop progress: "+str(cGrowth))
+file.write("Simulation started on the "+str(localtime)+"\n"+"Month;Year;Total Timesteps;Pops"+'\n')
+write_file(tts, P, month, year)
+print_data(month, year, P, bGrowth) #print to the Shell
 
 #Running Code
 while running == True:
@@ -51,7 +49,6 @@ while running == True:
     #Census
     bGrowth = default_growth*(pg+(gm-dm))
     round(bGrowth)
-
     if cGrowth >= 1000:
         n_growth=round(cGrowth/1000)
         cGrowth = cGrowth-(n_growth*1000)
@@ -59,44 +56,33 @@ while running == True:
     else:
         cGrowth = cGrowth+bGrowth
 
-    print("")
-
-    #Events
-    if events == True: #Events and similar things
-        #Reduce Pop Growth with larger populations
-        dm=(P/K)
+    print("")#To make printed output easy to read
     
-    #Time Keeper
+    if events == True: #Events and similar things
+        dm=(P/K)    #Reduce Pop Growth with larger populations
+    else:
+        pass
+    #Time Keeper Code
     if month == 12:
         month=1
         year=year+1
-
+        if year != end_year:
+            write_file(tts, P, month, year) #Write into save_file.txt
     else:
         month=month+1
 
     #Printer
     if year == end_year:
-        #Write into save_file.txt
-        file.write("Month: "+str(month)+" Year: "+ str(year)+';')
-        file.write("Population: "+";"+str(tts)+";"+ str(P)+'\n')
+        write_file(tts, P, month, year) #Write into save_file.txt
         file.write("Simulation has Ended"+'\n')
         localtime = time.asctime( time.localtime(time.time()) )
-        file.write("Simulation from the "+str(localtime))
+        file.write("Simulation ended on the "+str(localtime))
         file.close()
-        #print in the Shell
-        print("Month: "+str(month)+" Year: "+ str(year))
-        print("Population: " + str(P)+"K")
-        print("Growth: "+str(bGrowth))
+        print_data(month, year, P, bGrowth) #print to the Shell
         print("Simulation has Ended")
         running = False
     else:
-        #Write into save_file.txt
-        file.write("Month: "+str(month)+" Year: "+ str(year)+';')
-        file.write("Population: "+";"+str(tts)+";"+ str(P)+'\n')
-        #print in the Shell
-        print("Month: "+str(month)+" Year: "+ str(year))
-        print("Population: " + str(P)+"K")
-        print("Growth: "+str(bGrowth))
+        print_data(month, year, P, bGrowth) #print to the Shell
         if go_normal == True:
             time.sleep(1)
         if go_fast == True:
