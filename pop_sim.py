53315d074f0d9d56060067dcb8c433484f831465
German Nr4 <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2020-09-25 10:49:44 +0200

    Update 25/09/20
    -Updated >pop_sim.py<
    -Test agent based model in >code_test.py<

--- pop_sim.py
+++ pop_sim.py
@@ -1,7 +1,8 @@
 #Add imports here
 import time
-from random import *
+import random
 import csv
+import numpy as np
 
 #Simulation Status
 running = True
@@ -13,7 +14,7 @@ events = True
 tts = 1 #Totat Timesteps
 month = 1
 year = 1
-end_year = 10
+end_year = 100
 #Amount of Pops & related stuff; 1 Pop = 1k People
 P = 1
 cGrowth = 0
@@ -24,6 +25,9 @@ bGrowth = 100
 default_growth = 100
 n_growth = 0
 K = 50 #Supplies for Pops
+plague_timer = 0
+plague = False
+great_plague = False
 
 #Set-Up
 cGrowth = cGrowth+(bGrowth*(pg+gm-dm))
@@ -44,7 +48,21 @@ file.write("Simulation started on the "+str(localtime)+"\n"+"Month;Year;Total Ti
 output_data(month, year, tts, P, bGrowth)
 #Running Code
 while running == True:
+    if P <= 0:
+        running = False
+        print("ERROR")
     tts = tts+1
+    dm=(P/K)    #Reduce Pop Growth with larger populations
+    if plague == True:  #Plague Updater
+        plague_timer-=1
+        dm = dm+0.75
+        if plague_timer == 0:
+            plague = False
+    elif great_plague == True:
+        plague_timer-=1
+        P=P-1
+        if plague_timer == 0:
+            great_plague == False
     #Census
     bGrowth = default_growth*(pg+(gm-dm))
     round(bGrowth)
@@ -52,19 +70,40 @@ while running == True:
         n_growth=round(cGrowth/1000)
         cGrowth = cGrowth-(n_growth*1000)
         P=P+n_growth
+    elif cGrowth <= -1000:
+        n_growth=round(cGrowth/1000)
+        cGrowth = cGrowth-(n_growth*1000)
+        P=P-n_growth
     else:
         cGrowth = cGrowth+bGrowth
-    print("")#To make printed output easy to read
     if events == True: #Events and similar things
-        dm=(P/K)    #Reduce Pop Growth with larger populations
+        EventList = ["None"]*20 + ["P-Boom"]*5 + ["Plague"]*2
+        #+ ["Great Plague"]*1
+        event = random.choice(EventList)
+        if event == "None":
+            dm = 0
+            gm = 0
+            dm=(P/K)    #Reduce Pop Growth with larger populations
+        elif event == "Plague" and great_plague == False:
+            plague_timer = 6
+            plague = True
+            dm = dm+0.5
+        elif event == "Great Plague" and great_plague == False:
+            plague_timer = 5
+            great_plague = True
+        elif event == "P Boom" and plague == False:
+            gm = gm+1
     else:
         pass
+    
     #Time Keeper Code
     if month == 12:
         month=1
         year=year+1
         if year != end_year:
+            print_data(month, year, P, bGrowth)
             write_file(month, year, tts, P)
+            print("")#To make printed output easy to read
     else:
         month=month+1
     #Printer
@@ -77,7 +116,6 @@ while running == True:
         file.close()
         running = False
     else:
-        print_data(month, year, P, bGrowth)
         if go_normal == True:
             time.sleep(1)
         elif go_fast == True:
