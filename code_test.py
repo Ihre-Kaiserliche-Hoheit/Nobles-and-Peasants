7bfd448911814dad7d85a1c0caacf153db1e8ce2
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2020-09-28 20:10:05 +0200

    Update 28/09/20
    -Start of rewriting >pop_sim.py<, currently on hold
    -Progress in >code_test.py<
    -Thinking about name change of the project

--- code_test.py
+++ code_test.py
@@ -12,6 +12,8 @@ nameListF = ["Eva", "Nina", "Johanna", "Berbel", "Lisa"]
 sexList = ["Male", "Female"]
 name = ""
 sex = ""
+N = 4
+population = []
 
 class person(Agent):
     def __init__(self, unique_id, model):
@@ -22,10 +24,30 @@ class person(Agent):
         elif sex == "Female":
             name = random.choice(nameListF)
         self.name = name
+        #self.patronym = patronym
+        #self.lastname = lastname
         self.sex = sex
+        #self.father = father
+        #self.mother = mother
+    def setup(self):
+        pass
     def step(self):
         print("Hi, I'm "+str(self.name)+" and I'm "+str(self.sex)+" and my ID is "+str(self.unique_id))
-
+        #Chose two randoms
+        i = random.randint(0, len(population)-1)
+        j = random.randint(0, len(population)-1)
+        while i == j:
+            j = random.randint(0, len(population)-1)
+        #print(i) #Debug Code
+        #print(j) #Debug Code
+        i = population[i]
+        j = population[j]
+        while i.sex == j.sex:
+            j = random.randint(0, len(population)-1)
+            j = population[j]
+            print("new partner") #Debug Code
+        print(j.name) #Debug Code
+        print(i.name) #Debug Code
 class WorldModel(Model):
     def __init__(self, N):
         self.num_people = N
@@ -34,9 +56,10 @@ class WorldModel(Model):
         for i in range(self.num_people):
             #sex = random.choice(sexList)
             p = person(i, self)
-            self.schedule.add(p)
+            population.append(p)
+            self.schedule.add(p)    
     def step(self):
         self.schedule.step()
 
-run_model = WorldModel(5)
+run_model = WorldModel(N)
 run_model.step()
--- pop_sim.py
+++ pop_sim.py
@@ -1,21 +1,27 @@
-#Add imports here
+# Add imports here
+#Mesa Imports
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+#Other Imports
 import time
 import random
 import csv
 import numpy as np
 
-#Simulation Status
+# Simulation Status
 running = True
 go_normal = False
 go_fast = False
 go_very_fast = False
 events = True
-#Time
-tts = 1 #Totat Timesteps
+# Time
+tts = 1  # Total Timesteps
 month = 1
 year = 1
 end_year = 100
-#Amount of Pops & related stuff; 1 Pop = 1k People
+# Amount of Pops & related stuff; 1 Pop = 1k People
 P = 1
 cGrowth = 0
 pg = 1
@@ -24,103 +30,128 @@ gm = 0
 bGrowth = 100
 default_growth = 100
 n_growth = 0
-K = 50 #Supplies for Pops
+K = 50  # Supplies for Pops
 plague_timer = 0
 plague = False
 great_plague = False
 
-#Set-Up
-cGrowth = cGrowth+(bGrowth*(pg+gm-dm))
-bGrowth = default_growth*(pg+gm-dm)
-def write_file(month, year, tts, P):
-    #Write into save_file.txt
-    file.write(str(month)+";"+str(year)+";"+str(tts)+";"+ str(P)+'\n')
-def print_data(month, year, P, bGrowth):
-    print("Month: "+str(month)+" Year: "+ str(year)+"\n"+"Population: " + str(P)+"K"+"\n"+"Growth: "+str(bGrowth))
-def output_data(month, year, tts, P, bGrowth):
-    #Write into save_file.txt
-    file.write(str(month)+";"+str(year)+";"+str(tts)+";"+ str(P)+'\n')
-    print("Month: "+str(month)+" Year: "+ str(year)+"\n"+"Population: " + str(P)+"K"+"\n"+"Growth: "+str(bGrowth))
+# Set-Up
+cGrowth = cGrowth + (bGrowth * (pg + gm - dm))
+bGrowth = default_growth * (pg + gm - dm)
 
-localtime = time.asctime( time.localtime(time.time()) )
-file =open ("save_file.csv", "w", newline='')
-file.write("Simulation started on the "+str(localtime)+"\n"+"Month;Year;Total Timesteps;Pops"+'\n')
+class pop(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.job = job
+    def get_job(self):
+        pass
+    def step(self):
+        print("Pop: "+str(self.unique_id))
+
+class PopModel(Model):
+    def __init__(self):
+        self.schedule = RandomActivation(self)
+    def step(self):
+        self.schedule.step()
+
+def write_file(month, year, tts, P):
+    # Write into save_file.txt
+    file.write(str(month) + ";" + str(year) + ";" + str(tts) + ";" + str(P) + '\n')
+
+
+def print_data(month, year, P, bGrowth):
+    print(
+        "Month: " + str(month) + " Year: " + str(year) + "\n" + "Population: " + str(P) + "K" + "\n" + "Growth: " + str(
+            bGrowth))
+
+
+def output_data(month, year, tts, P, bGrowth):
+    # Write into save_file.txt
+    file.write(str(month) + ";" + str(year) + ";" + str(tts) + ";" + str(P) + '\n')
+    print(
+        "Month: " + str(month) + " Year: " + str(year) + "\n" + "Population: " + str(P) + "K" + "\n" + "Growth: " + str(
+            bGrowth))
+
+
+localtime = time.asctime(time.localtime(time.time()))
+file = open("save_file.csv", "w", newline='')
+file.write("Simulation started on the " + str(localtime) + "\n" + "Month;Year;Total Timesteps;Pops" + '\n')
 output_data(month, year, tts, P, bGrowth)
-#Running Code
-while running == True:
+# Running Code
+while running is True:
     if P <= 0:
         running = False
         print("ERROR")
-    tts = tts+1
-    dm=(P/K)    #Reduce Pop Growth with larger populations
-    if plague == True:  #Plague Updater
-        plague_timer-=1
-        dm = dm+0.75
+    tts = tts + 1
+    dm = (P / K)  # Reduce Pop Growth with larger populations
+    if plague is True:  # Plague Updater
+        plague_timer -= 1
+        dm = dm + 0.75
         if plague_timer == 0:
             plague = False
-    elif great_plague == True:
-        plague_timer-=1
-        P=P-1
+    elif great_plague is True:
+        plague_timer -= 1
+        P = P - 1
         if plague_timer == 0:
-            great_plague == False
-    #Census
-    bGrowth = default_growth*(pg+(gm-dm))
+            great_plague = False
+    # Census
+    bGrowth = default_growth * (pg + (gm - dm))
     round(bGrowth)
     if cGrowth >= 1000:
-        n_growth=round(cGrowth/1000)
-        cGrowth = cGrowth-(n_growth*1000)
-        P=P+n_growth
+        n_growth = round(cGrowth / 1000)
+        cGrowth = cGrowth - (n_growth * 1000)
+        P = P + n_growth
     elif cGrowth <= -1000:
-        n_growth=round(cGrowth/1000)
-        cGrowth = cGrowth-(n_growth*1000)
-        P=P-n_growth
+        n_growth = round(cGrowth / 1000)
+        cGrowth = cGrowth - (n_growth * 1000)
+        P = P - n_growth
     else:
-        cGrowth = cGrowth+bGrowth
-    if events == True: #Events and similar things
-        EventList = ["None"]*20 + ["P-Boom"]*5 + ["Plague"]*2
-        #+ ["Great Plague"]*1
+        cGrowth = cGrowth + bGrowth
+    if events is True:  # Events and similar things
+        EventList = ["None"] * 20 + ["P-Boom"] * 5 + ["Plague"] * 2
+        # + ["Great Plague"]*1
         event = random.choice(EventList)
         if event == "None":
             dm = 0
             gm = 0
-            dm=(P/K)    #Reduce Pop Growth with larger populations
-        elif event == "Plague" and great_plague == False:
+            dm = (P / K)  # Reduce Pop Growth with larger populations
+        elif event == "Plague" and great_plague is False:
             plague_timer = 6
             plague = True
-            dm = dm+0.5
-        elif event == "Great Plague" and great_plague == False:
+            dm = dm + 0.5
+        elif event == "Great Plague" and great_plague is False:
             plague_timer = 5
             great_plague = True
-        elif event == "P Boom" and plague == False:
-            gm = gm+1
+        elif event == "P Boom" and plague is False:
+            gm = gm + 1
     else:
         pass
-    
-    #Time Keeper Code
+
+    # Time Keeper Code
     if month == 12:
-        month=1
-        year=year+1
+        month = 1
+        year = year + 1
         if year != end_year:
             print_data(month, year, P, bGrowth)
             write_file(month, year, tts, P)
-            print("")#To make printed output easy to read
+            print("")  # To make printed output easy to read
     else:
-        month=month+1
-    #Printer
+        month = month + 1
+    # Printer
     if year == end_year:
         output_data(month, year, tts, P, bGrowth)
         print("Simulation has Ended")
-        localtime = time.asctime( time.localtime(time.time()))
-        file.write("Simulation has Ended"+'\n'+"Simulation ended on the "+str(localtime))
-        #file.write("Simulation ended on the "+str(localtime))
+        localtime = time.asctime(time.localtime(time.time()))
+        file.write("Simulation has Ended" + '\n' + "Simulation ended on the " + str(localtime))
+        # file.write("Simulation ended on the "+str(localtime))
         file.close()
         running = False
     else:
-        if go_normal == True:
+        if go_normal is True:
             time.sleep(1)
-        elif go_fast == True:
+        elif go_fast is True:
             time.sleep(0.5)
-        elif go_very_fast == True:
+        elif go_very_fast is True:
             time.sleep(0.25)
         else:
             pass
