095ba9587d5a6aa47a6ae190c236064fd0edc7c1
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2020-09-30 19:49:15 +0200

    Update 30/09/20
    -Fixed a bug in the spouse finding code and added additional limits to who can marry who

--- code_test.py
+++ code_test.py
@@ -9,15 +9,18 @@ import random
 import csv
 
 nameListM = ["Heinrich", "Reinhardt", "Adam", "Peter", "Günter", "Johan", "Hans", "Franz", "Friedrich", "Uwe", "Johannes", "Michael", "Wilhelm"]
-nameListF = ["Eva", "Nina", "Johanna", "Berbel", "Lisa", "Frida", "Wilhelmina", "Svenja", "Sonja", "Marie", "Maria", "Uta", "Hildegard"]
+nameListF = ["Eva", "Nina", "Johanna", "Berbel", "Lisa", "Frida", "Wilhelmina", "Svenja", "Sonja", "Marie", "Maria", "Uta", "Hildegard", "Lena"]
 sexList = ["Male", "Female"]
 name = ""
 sex = ""
 N = 5
 population = []
-father = ""
-mother = ""
-alive = True
+potential_partners = []
+#father = []
+#father_id = ""
+#mother = []
+#mother_id = ""
+#alive = True
 age = 0
 
 class person(Agent):
@@ -29,13 +32,17 @@ class person(Agent):
         elif sex == "Female":
             name = random.choice(nameListF)
         self.name = name
+        #self.patronym = ""
         #self.lastname = lastname
         self.sex = sex
         self.age = age
-        self.father = father
-        self.mother = mother
-        self.spouse = ""
-        self.alive = alive
+        self.father = []
+        self.father_id = ""
+        self.mother = []
+        self.mother_id = ""
+        self.spouse = []
+        self.spouse_id = ""
+        self.alive = True
     def age_update(self):
         age = self.age
         age = age+1
@@ -54,6 +61,8 @@ class person(Agent):
                 if random.randint(0, 100) < 50:
                     self.alive = False
                     run_model.schedule.remove(self)
+    def limit_partner(self, population):
+        pass
     def find_partner(self, population):
         #Chose two randoms
         i = random.randint(0, len(population)-1)
@@ -72,54 +81,66 @@ class person(Agent):
             j = population[j]
             sg = sg+1
             #print("new partner 1") #Debug Code
-        while j.spouse != "" and sg <= 10:
+        while j.spouse_id != "" and sg <= 10:
             j = random.randint(0, len(population)-1)
             j = population[j]
             sg = sg+1
             #print("new partner 2") #Debug Code
-        while i.spouse != "" and sg <= 10:
+        while i.spouse_id != "" and sg <= 10:
             i = random.randint(0, len(population)-1)
             i = population[i]
             sg = sg+1
             #print("new partner 3") #Debug Code
-        if i.sex == j.sex and sg >= 10:
-            if i.spouse != "":
-                j.spouse = ""
-            elif j.spouse != "":
-                i.spouse = ""
+        if i.sex == j.sex:
+            if i.spouse_id != "":
+                j.spouse_id = ""
+            elif j.spouse_id != "":
+                i.spouse_id = ""
             else:
                 pass
+        elif i.spouse_id == i.unique_id or j.spouse_id == j.unique_id:
+            if i.spouse_id == i.unique_id:
+                i.spouse_id = ""
+            elif j.spouse_id == j.unique_id:
+                j.spouse_id = ""
+        elif i.age >= 50 or j.age >= 50:
+            if i.age >= 50:
+                i.spouse_id = ""
+                j.spouse_id = ""
+            elif j.age >= 50:
+                i.spouse_id = ""
+                j.spouse_id = ""
         else:
-            i.spouse = j.unique_id
-            j.spouse = i.unique_id
+            i.spouse_id = j.unique_id
+            j.spouse_id = i.unique_id
         sg = 0
-        if j.spouse == i.unique_id and i.spouse == j.unique_id:
+        #if j.spouse_id == i.unique_id and i.spouse_id == j.unique_id:
             #print(str(i.unique_id)+" is married to "+str(j.unique_id))
-            pass
-        else:
-            pass
+            #pass
+        #else:
+            #pass
     def have_kid(self):
-        mother = ""
-        father = ""
+        mother_id = ""
+        father_id = ""
         if self.sex == "Male":
-            father = self.unique_id
-            mother = self.spouse
+            father_id = self.unique_id
+            mother_id = self.spouse_id
         elif self.sex == "Female":
-            father = self.spouse
-            mother = self.unique_id
+            father_id = self.spouse_id
+            mother_id = self.unique_id
         i = len(population)
         p = person(i, self)
-        p.father = father
-        p.mother = mother
+        p.father_id = father_id
+        p.mother_id = mother_id
         population.append(p)
         #print(str(p.unique_id)+" was born to "+str(father)+" and "+str(mother))
         run_model.schedule.add(p)
     def step(self):
         if self.alive == True:
             if self.age >= 16:
-                if self.spouse == "" or self.spouse == -1:
+                if self.spouse_id == "" or self.spouse_id == -1:
                     self.find_partner(population)
-                if self.spouse != "":
+                if self.spouse_id != "":
                     self.have_kid()
             self.age_update()
         elif self.alive == False:
@@ -143,8 +164,9 @@ run_model = WorldModel(N)
 file = open("save_file_2.csv", "w", newline="")
 for i in range(50):
     run_model.step()
-    #print("")
+    #print("\n"+"Total Population: "+str(len(population)))
+file.write("Unique ID; Name; Sex; Father ID; Mother ID; Spouse ID"+"\n")
 for i in population:
-    file.write(str(i.unique_id)+";"+str(i.name)+";"+str(i.sex)+";"+str(i.father)+";"+str(i.mother)+";"+str(i.spouse)+"\n")
+    file.write(str(i.unique_id)+";"+str(i.name)+";"+str(i.sex)+";"+str(i.father_id)+";"+str(i.mother_id)+";"+str(i.spouse_id)+"\n")
 file.close()
 print("\n""The End | Total Population: "+str(len(population)))
