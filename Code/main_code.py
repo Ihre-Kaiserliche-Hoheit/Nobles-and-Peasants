46401b8cb86e63614ef6585a055e033f7024ab9d
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2021-05-03 20:28:44 +0200

    1.2 - 'A happy little Family'
    - People are now less likely to marry their cousins
    
    - Improved code where I could
    
    - Tweaked some numbers
    
    - Prayed to the Machine Spirit that no bugs where introduced
    
    -Note: making the cousin system work was a fucking pain in the ass because it feels like I could optimize it somehow more

--- Code/main_code.py
+++ Code/main_code.py
@@ -1,7 +1,9 @@
 #TODO:
 #   Fix any remaining bugs
 #   Fine tune systems
-
+#
+#   Version ?.?
+#       System that auto prunes the family tree before converting it into a .ged?
 
 #NOTE:
 #I likely don't know either how half of this works in a few weeks - Kaiser
@@ -21,10 +23,10 @@ Seeder = str(time.strftime("%Y.%m.%d %H:%M:%S ")) #Seed for the randomness of th
 r.seed(Seeder) #Set seed for repeatable results
 
 #Change values to the setting ones
+printing = True
+
 settings = fc.txt_to_list("../Input/Settings.txt")
 settings.pop(0)
-
-printing = False
 seed = int(settings[0])
 k = int(settings[1])
 life_expecantcy_avg = int(settings[2])
@@ -69,6 +71,7 @@ class person():
         self.birth_date = None
         self.age = 0
         self.death_date = None
+        self.alive = True
 
         self.father = []
         self.mother = []
@@ -85,10 +88,6 @@ class person():
             self.post_pregnancy_break -=1
             if rand_value < preg_tweak*kr:
                 self.have_kid(self, self.spouse[0])
-
-        #if self.surname == "":
-         #   self.surname = r.choices(lastnames)
-         #   self.surname = self.surname[0]
         
         self.death() #Looks if they die today
 
@@ -98,42 +97,20 @@ class person():
         child.uid = len(total_population)
         
         #Add parent-child relations
-        child.mother.append(mother)
-        mother.children.append(child)
-        child.father.append(father)
-        father.children.append(child)
+        parent_child(mother, child)
+        parent_child(father, child)
 
         #Set values
         child.birth_date = year
-        add_to_population(child)
         child.sex = r.randint(0, 1)
-        set_name(child)
-        ran = r.randint(0, 10)
-        if -1 < ran < 3:
-            child.surname = r.choices(lastnames)
-            child.surname = child.surname[0]
-        elif 3 < ran < 9:
-            child.surname = father.surname
-            if child.surname == "":
-                child.surname = r.choices(lastnames)
-                child.surname = child.surname[0]
-        elif 9 < ran < 11 and "-" not in father.surname and "-" not in mother.surname:
-            child.surname = str(father.surname) + "-" + str(mother.surname)
-            if child.surname == "-":
-                child.surname = r.choices(lastnames)
-                child.surname = child.surname[0]
-        else:
-            child.surname = r.choices(lastnames)
-            child.surname = child.surname[0]
+        set_whole_name(child)
 
-        if child.surname.startswith("-"):
-            child.surname = child.surname.strip("-")
+        add_to_population(child)
         mother.post_pregnancy_break = r.randint(1, 3)
 
     def have_kid(self, mother, father):
         if mother.post_pregnancy_break <= 0:
             mother.birth(mother, father)
-        pass
 
     def death(self):
         age_dif = self.age - life_expecantcy_avg #Gets how far above/below they are
@@ -146,11 +123,18 @@ class person():
 
 
 #All the other stuff
+def parent_child(parent, child):
+    parent.children.append(child)
+    if parent.sex == 0:
+        child.father.append(parent)
+    else:
+        child.mother.append(parent)
+    
 def update_avaiblity_lists():
     #Goes through the lists for avaible men and women, may be improved for better performance
     for i in range(len(living_population)):
         person = living_population[i]
-        if len(person.spouse) == 0 and (person not in avaible_males and person not in avaible_females) and person.age < 40:
+        if len(person.spouse) == 0 and (person not in avaible_males and person not in avaible_females) and person.age < 40 and person.alive == True:
             if person.sex == 0:
                 avaible_males.append(person)
             elif person.sex == 1:
@@ -161,17 +145,6 @@ def update_avaiblity_lists():
 
             elif (40 < person.age or 0 < len(person.spouse)) and person.sex == 1:
                 avaible_females.remove(person)
-    remove_dead(avaible_males)
-    remove_dead(avaible_females)
-
-def remove_dead(List:list):
-    try:
-        for i in range(len(List)):
-            person = List[i]
-            if person not in living_population:
-                List.remove(person)
-    except IndexError:
-        pass #Can be ignored? Could produce bugs, or maybe not
 
 def create_random_list_of_uniques(List:list, amount:int):
     #Cuz I don't give a fuck if that is already possible with the random lib
@@ -190,7 +163,6 @@ def create_random_list_of_uniques(List:list, amount:int):
 
 def find_spouse(searcher):
     pool_of_possible_spouses = []
-    
     #Creates a ~hopefully~ fully unqiue list of people
     if searcher.sex == 0:
         pool_of_possible_spouses = create_random_list_of_uniques(avaible_females, 20)
@@ -223,7 +195,6 @@ def marrige(partner1, partner2):
 
 def assigne_spouse_value(searcher, other):
     value = 0
-
     #Add new factors for spouse choice here
     value = value + asses_age(other) + asses_relation(searcher, other)
 
@@ -233,7 +204,7 @@ def asses_age(other):
     #Older == Less likely to marry
     value = 0
     if 35 < other.age:
-        value = value - 7
+        value = value - 25
 
     elif other.age < 25:
         value += 10
@@ -243,59 +214,112 @@ def asses_age(other):
 
     return(value)
 
-def asses_relation(point1, point2):
+def asses_relation(p1, p2):
     #Most people don't want to marry their siblings...
     value = 0
-    check = False
-
     try:
-        f1 = point1.father[0]
-        f2 = point2.father[0]
-        m1 = point1.mother[0]
-        m2 = point2.mother[0]
-        check = True
+        if is_sibling(p1, p2) == True:
+            #Yikes, we ain't in Alabama
+            value = -50
 
-    except IndexError:
-        # ... and people tend to prefer unreleated people...
-        value +=5
-        return(value)
+        elif is_cousin(p1, p2) == True:
+            #Habsburg, get the fuck out
+            value = -25
 
-    if f1 == f2 or m1 == m2:
-        #... except in rare cases where everyone else is worse.
-        value = -50
+        else:
+            value = 10 #Should always be identical to the value in the except-block
+
+    except IndexError: #Because the first gen has no parents
+        value = 10
         
     #Maybe add more complex system for more distant relatives
     return(value)
-    
+
+def is_cousin(p1, p2):
+    #Checks if p1 and p2 are cousins or not
+    #I doubt this is the best way to do this function but I have no clue how to do it better
+    f1 = p1.father[0]
+    f2 = p2.father[0]
+    m1 = p1.mother[0]
+    m2 = p2.mother[0]
+
+    if is_sibling(f1, f2) == True or is_sibling(m1, m2) == True or is_sibling(f1, m2) == True or is_sibling(f2, m1) == True:
+        return True
+    else:
+        return False
+
+def is_sibling(p1, p2):
+    #Checks if p1 and p2 are siblins or not
+    if p1.father[0] == p2.father[0] or p1.mother[0] == p2.mother[0]:
+        return True
+    else:
+        return False
+
 def from_thin_air(person):
     #For the set up only
     #Create child
     person.uid = len(total_population)
-
     #Set values
-    person.birth_date = year-20
-    person.age = 20
-    add_to_population(person)
+    rage = r.randint(17, 23)
+    person.birth_date = year-rage
+    person.age = rage
     person.sex = r.randint(0, 1)
-    set_name(person)
-    person.surname = r.choices(lastnames)
-    person.surname = person.surname[0]
-    #Seed population code goes here
+    set_whole_name(person)
+    #Add to population lists
+    add_to_population(person)
 
 def death(person):
     #U ded
     person.death_date = year
+    person.alive = False
     living_population.remove(person)
 
-def set_name(person):
-    if person.sex == 0:
-        person.name = r.choices(male_names)
+def set_name(sex):
+    name = ""
+    if sex == 0:
+        name = r.choices(male_names)
 
     else:
-        person.name = r.choices(female_names)
+        name = r.choices(female_names)
     #Maybe adda list of gender-neutral names for the case this code fucks up? - Kaiser
     #Nah, this is fine - Steve
+    name = name[0]
+    return(name)
 
+def set_surname(child, father, mother):
+    ran = r.randint(0, 10)
+    surname = ""
+    
+    if 3 < ran < 9:
+        #Most people just take the lastname of their father
+        surname = father.surname
+        
+    elif 9 < ran < 11 and "-" not in father.surname and "-" not in mother.surname:
+        #Some have both their mothers and fathers lastname
+        surname = str(father.surname) + "-" + str(mother.surname)
+        
+    if surname.startswith("-"):
+        #Some have lastnames that start weirdly and need to be PURGED
+        surname = surname.strip("-")
+
+    if surname == "":
+        #And some are just poor bastards without a lastname
+        surname = random_surname()
+
+    return(surname)
+
+def random_surname():
+    surname = r.choices(lastnames)
+    surname = surname[0]
+    return(surname)
+
+def set_whole_name(person):
+    person.name = set_name(person.sex)
+    try:
+        person.surname = set_surname(person, person.father[0], person.mother[0])
+    except IndexError:
+        person.surname = random_surname()
+    
 def add_to_population(person):
     #Add to both lists, cuz it is easy to have one function that does both for me
     total_population.append(person)
@@ -318,7 +342,9 @@ for i in range(seed):
     #Create starting population
     new_person = person()
     from_thin_air(new_person)
-       
+
+#census = open("census.csv", "w")
+
 while year < end_year:
     #Simulation cycle after the creation
     year +=1
@@ -326,13 +352,15 @@ while year < end_year:
     r.shuffle(queue)
     p = len(living_population)
     try:
-        kr = round(1-(p/k), 8)
+        kr = round(1-((p/k)*0.8), 8)
     except ZeroDivisionError:
         pass
+    #census.write(str(p)+"\n")
     if printing == True:
         print("Year " + str(year))
         print(len(living_population))
     update_avaiblity_lists()
+    base_infant_mortality = base_infant_mortality*0.9 #Revise if tech is ever added
     ii = 0
     while ii < len(living_population):
         c = living_population[ii]
@@ -367,10 +395,11 @@ for i in range(len(total_population)):
         spouse_id = spouse.uid
     except IndexError:
         spouse_id = ""
-    file.write(str(person.uid) + ";" + str(person.name[0])  + ";" + str(person.surname)  + ";" + str(person.sex) + ";" + str(father_id)  + ";" + str(mother_id) + ";" + str(spouse_id) + ";" + str(id_list) + ";" + str(person.birth_date) + ";" + str(person.death_date) + ";" + str(person.age) + "; \n")
+    file.write(str(person.uid) + ";" + str(person.name)  + ";" + str(person.surname)  + ";" + str(person.sex) + ";" + str(father_id)  + ";" + str(mother_id) + ";" + str(spouse_id) + ";" + str(id_list) + ";" + str(person.birth_date) + ";" + str(person.death_date) + ";" + str(person.age) + "; \n")
 
 file.close()
 gc.converter()
+#census.close()
 
 #Clean up, delets raw_output and moves the gedcom to the output folder
 fc.delete_file("raw_output.txt")
